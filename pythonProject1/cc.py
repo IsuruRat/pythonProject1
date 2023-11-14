8bbb607d340c4b148ce89a426df63d957e072bd0
IsuruRat <isurumrathnayaka@gmail.com>
2023-11-14 20:09:15 +0530

    Commit message

---
+++ pythonProject1/cc.py
@@ -0,0 +1,45 @@
+#exercise 1 prime nu checker
+# def prime_checker(number):
+#     is_prime = True
+#
+#     for i in range(2, number):
+#         if number % i == 0:
+#             is_prime = False
+#             #print("not a prime number")
+#     # if this for statement never triggered that means it is a  prime number
+#     if is_prime:    #if is_prime == True: me dekama ekai
+#         print("it is a prime")
+#
+#     else: print("it is not prime")
+#
+# prime_checker(7)
+
+#EX2
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+    #e.g.
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+
+
+
+
+
+
--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,3 +1,27 @@
-print("issa")
-print("Gtithux")
-print("hshdgd")
+h = int(input("enter your height"))
+
+if h > 120:
+    print("you can ride")
+    age = int(input("what is your age?"))
+    p = int(input("want a photo?"))
+    if age <= 12:
+        if p == 1:
+            print("pay 8")
+        else:
+            print("pay 5")
+
+    elif age > 1:
+        if p == 10:
+            print("pay 15")
+        else:
+            print("pay 12")
+
+    else:
+        if p == 1:
+            print("pay 10")
+        else:
+            print("pay 7")
+
+else:
+    print("you cant ride")
+
